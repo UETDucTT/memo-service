8b66754f50b6f2af6845f6c2d120fef374125760
anhducc13 <trantienduc10@gmail.com>
2021-01-07 23:50:28 +0700

    update

--- src/migration/1609951183365-add_preview_link.ts
+++ src/migration/1609951183365-add_preview_link.ts
@@ -5,7 +5,7 @@ export class addPreviewLink1609951183365 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      'CREATE TABLE `link` (`id` varchar(36) NOT NULL, `url` text NOT NULL, `image` text NULL, `title` text NULL, `description` text NULL, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), `diaryId` varchar(36) NULL, PRIMARY KEY (`id`)) COLLATE "utf8mb4_0900_ai_ci" ENGINE=InnoDB',
+      'CREATE TABLE `link` (`id` varchar(36) NOT NULL, `url` text NOT NULL, `image` text NULL, `title` text NULL, `description` text NULL, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), `diaryId` varchar(36) NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
     );
     await queryRunner.query(
       'ALTER TABLE `link` ADD CONSTRAINT `FK_6ef8a5731999c136796192417d0` FOREIGN KEY (`diaryId`) REFERENCES `diary`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION',
