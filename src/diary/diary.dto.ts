64c9f14833ede0b0cc8f9baace43a981513ca72a
anhducc13 <trantienduc10@gmail.com>
2021-01-17 23:13:41 +0700

    update

--- src/diary/diary.dto.ts
+++ src/diary/diary.dto.ts
@@ -188,22 +188,22 @@ export class SearchDiaryDto {
 
   @ApiProperty({ required: false, default: 1 })
   @IsOptional()
-  @Transform(val => {
-    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
-      throw new BadRequestException('page validation fail');
-    }
-    return parseInt(val?.toString());
-  })
+  // @Transform(val => {
+  //   if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
+  //     throw new BadRequestException('page validation fail');
+  //   }
+  //   return parseInt(val?.toString());
+  // })
   page: number;
 
   @ApiProperty({ required: false, default: 10 })
   @IsOptional()
-  @Transform(val => {
-    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
-      throw new BadRequestException('pageSize validation fail');
-    }
-    return parseInt(val?.toString());
-  })
+  // @Transform(val => {
+  //   if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
+  //     throw new BadRequestException('pageSize validation fail');
+  //   }
+  //   return parseInt(val?.toString());
+  // })
   pageSize: number;
 
   @ApiProperty({ required: false })
--- src/notification/notification.dto.ts
+++ src/notification/notification.dto.ts
@@ -6,22 +6,22 @@ import { IsOptional, IsUUID } from 'class-validator';
 export class SearchNotificationDto {
   @ApiProperty({ required: false, default: 1 })
   @IsOptional()
-  @Transform(val => {
-    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
-      throw new BadRequestException('page validation fail');
-    }
-    return parseInt(val?.toString());
-  })
+  // @Transform(val => {
+  //   if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
+  //     throw new BadRequestException('page validation fail');
+  //   }
+  //   return parseInt(val?.toString());
+  // })
   page: number;
 
   @ApiProperty({ required: false, default: 10 })
   @IsOptional()
-  @Transform(val => {
-    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
-      throw new BadRequestException('pageSize validation fail');
-    }
-    return parseInt(val?.toString());
-  })
+  // @Transform(val => {
+  //   if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
+  //     throw new BadRequestException('pageSize validation fail');
+  //   }
+  //   return parseInt(val?.toString());
+  // })
   pageSize: number;
 
   @ApiProperty({ required: false })
