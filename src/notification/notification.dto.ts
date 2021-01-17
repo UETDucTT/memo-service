42d76df9856b4aeb334af7dfe47bbec8b5cc960b
anhducc13 <trantienduc10@gmail.com>
2021-01-17 22:56:54 +0700

    updtae

--- src/notification/notification.dto.ts
+++ src/notification/notification.dto.ts
@@ -7,20 +7,20 @@ export class SearchNotificationDto {
   @ApiProperty({ required: false, default: 1 })
   @IsOptional()
   @Transform(val => {
-    if (isNaN(parseInt(val)) || parseInt(val) <= 0) {
+    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
       throw new BadRequestException('page validation fail');
     }
-    return parseInt(val);
+    return parseInt(val?.toString());
   })
   page: number;
 
   @ApiProperty({ required: false, default: 10 })
   @IsOptional()
   @Transform(val => {
-    if (isNaN(parseInt(val)) || parseInt(val) <= 0) {
+    if (isNaN(parseInt(val?.toString())) || parseInt(val?.toString()) <= 0) {
       throw new BadRequestException('pageSize validation fail');
     }
-    return parseInt(val);
+    return parseInt(val?.toString());
   })
   pageSize: number;
 
