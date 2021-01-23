c0f2edb9a9e91cbdf1583a12af623d749bcdc59e
anhducc13 <trantienduc10@gmail.com>
2021-01-23 22:51:10 +0700

    update

--- src/migration/1611415406596-diary_share.ts
+++ src/migration/1611415406596-diary_share.ts
@@ -8,7 +8,7 @@ export class diaryShare1611415406596 implements MigrationInterface {
       'CREATE TABLE `diary_share` (`id` varchar(36) NOT NULL, `email` varchar(255) NOT NULL, `createdAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6), `updatedAt` timestamp(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6), `diaryId` varchar(36) NULL, PRIMARY KEY (`id`)) ENGINE=InnoDB',
     );
     await queryRunner.query(
-      'ALTER TABLE `diary_share` CHANGE COLUMN `diaryId` `diaryId` VARCHAR(36) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL',
+      'ALTER TABLE `diary_share` CHANGE COLUMN `diaryId` `diaryId` VARCHAR(36) CHARACTER SET utf8mb4 NULL DEFAULT NULL',
     );
     await queryRunner.query(
       'ALTER TABLE `diary_share` ADD CONSTRAINT `FK_919f9f620c1820dc9bf2e465217` FOREIGN KEY (`diaryId`) REFERENCES `diary`(`id`) ON DELETE CASCADE ON UPDATE NO ACTION',
