23c705526b10174d9e8427daf80a9c1f8ea30f4d
anhducc13 <trantienduc10@gmail.com>
2021-01-31 16:22:38 +0700

    update

--- src/main.ts
+++ src/main.ts
@@ -38,7 +38,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
 
   SwaggerModule.setup('api', app, document);
-  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
